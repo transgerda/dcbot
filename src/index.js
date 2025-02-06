127ddc9e644c76a8a2684aa0a928a903cb43c8de
Martijn van Boven <info@martijnvanboven.nl>
2025-02-06 10:32:02 +0100

    comments index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
-const { ActionRowBuilder, ButtonBuilder } = require('@discordjs/builders');
-const { Client, IntentsBitField, ButtonStyle  } = require('discord.js');
+const { Client, IntentsBitField } = require('discord.js');
 
 const dcbot = new Client({
   intents: [
@@ -19,19 +18,23 @@ dcbot.on('ready', () => {
   console.log('✅ The bot is ready')
 })
 
+
+// slash command interactions
 dcbot.on('interactionCreate', (interaction) =>  {
   if (!interaction.isChatInputCommand()) return;
 
-    if (interaction.commandName === 'yusu') {
-      interaction.reply('Yusu indeed');
-    }
-  
-    if (interaction.commandName === 'prompt') {
-      interaction.reply(interaction.options.get('prompt').value);
-    }
-  })
+  // yusu command
+  if (interaction.commandName === 'yusu') {
+    interaction.reply('Yusu indeed');
+  }
 
- 
+  // prompt command (let the bot say anything, later A.I. prompt)
+  if (interaction.commandName === 'prompt') {
+    interaction.reply(interaction.options.get('prompt').value);
+  }
+})
+
+// role interactions 
 dcbot.on('interactionCreate', async (interaction) => {
   const allRolls = ['1336401515493068883', '1336402852159029370','1336402877027188848', '1336402908614361118', '1336402949726797996'];
   
@@ -41,6 +44,7 @@ dcbot.on('interactionCreate', async (interaction) => {
     
     const customId = interaction.customId;
 
+    // if 'geen!' is clicked then remove all roles
     if (customId === '0') {
       allRolls.forEach(async (role) => {
         await interaction.member.roles.remove(allRolls);
@@ -49,6 +53,7 @@ dcbot.on('interactionCreate', async (interaction) => {
       return;
     };
     
+    // if 'allemaal!' is clicked then add all roles
     if (customId === '1') {
       allRolls.forEach(async (role) => {
         await interaction.member.roles.add(allRolls);
@@ -58,7 +63,7 @@ dcbot.on('interactionCreate', async (interaction) => {
     };
     
     const role = interaction.guild.roles.cache.get(interaction.customId);
-
+    // check if the role exist or if it isn't removed
     if (!role) { 
       interaction.editReply({
         content: 'Kan het vak niet vinden! vertel Martijn',
@@ -68,17 +73,20 @@ dcbot.on('interactionCreate', async (interaction) => {
 
     const hasRole = interaction.member.roles.cache.has(role.id);
     
+    // if the member already has the role then remove it
     if (hasRole) {
       await interaction.member.roles.remove(role);
       await interaction.editReply(`${role.name} is verwijderd van je profiel.`);
       return;
+    } else {
+      // else add it 
+      await interaction.member.roles.add(role);
+      await interaction.editReply(`${role.name} is toegevoegd aan je profiel.`);
     }
-  
-    await interaction.member.roles.add(role);
-    await interaction.editReply(`${role.name} is toegevoegd aan je profiel.`);
   } catch (e) {
     console.log(e);
   }
 })
 
+// make the bot online
 dcbot.login(process.env.TOKEN);
